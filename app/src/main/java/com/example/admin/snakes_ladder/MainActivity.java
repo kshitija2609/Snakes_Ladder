03ccb82d41e805d482b2f2ec94657837b47c8387
kshitija2609 <kshitijashankarpure@gmail.com>
2018-11-24 18:03:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/admin/snakes_ladder/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.admin.snakes_ladder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    int[] myDices = {
+            R.drawable.one,
+            R.drawable.two,
+            R.drawable.three,
+            R.drawable.four,
+            R.drawable.five,
+            R.drawable.six
+
+
+    };
+
+
+
+}
